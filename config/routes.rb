19ff21a88e648b40e5969247bd97b809aa1ee8db
David Lowndes <david.lowndes@firstb2b.com>
2016-09-23 15:16:04 +0100

    Reset root route, added my_ideas route, updated nav a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#my_ideas'
+  
+  root 'pages#home'
+  get 'my_ideas', to: 'users#my_ideas'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
