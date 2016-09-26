fabc0f59b17f9f3fff4c92d018c05dd008887c25
David Lowndes <david.lowndes@firstb2b.com>
2016-09-26 11:14:30 +0100

    Added voting style attribute to ideas, partial'ed idea index

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923144859) do
+ActiveRecord::Schema.define(version: 20160926095009) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "user_id"
+    t.string   "voting_style", default: "Binary"
   end
 
   create_table "users", force: :cascade do |t|
