fceaae44da04d61f800bf003a3496804c9b3a836
David Lowndes <david.lowndes@firstb2b.com>
2016-09-27 15:46:35 +0100

    Added ability to choose if comments on an idea will be anonymous or not

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926145745) do
+ActiveRecord::Schema.define(version: 20160927143747) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -22,13 +22,14 @@ ActiveRecord::Schema.define(version: 20160926145745) do
     t.integer  "user_id"
     t.index ["idea_id"], name: "index_comments_on_idea_id"
   end
-
+  
   create_table "ideas", force: :cascade do |t|
     t.string   "text"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.integer  "user_id"
-    t.string   "voting_style", default: "Binary"
+    t.string   "voting_style",       default: "Binary"
+    t.integer  "anonymous_comments", default: 0
   end
 
   create_table "users", force: :cascade do |t|
