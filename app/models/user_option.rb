bf5ff1f03cfe37ee8ae73335dd406b3b8f25867e
David Lowndes <david.lowndes@firstb2b.com>
2016-10-03 16:14:40 +0100

    It took me all day but user options appear to fully work

--- app/models/user_option.rb
+++ app/models/user_option.rb
@@ -1,3 +1,15 @@
 class UserOption < ApplicationRecord
   belongs_to :user, dependent: :destroy
+  
+  def anonymous_comments_default?
+    anonymous_comments_default == 1
+  end
+  
+  def real_time_voting_default?
+    real_time_voting_default == 1
+  end
+  
+  def reveal_voter_details_default? 
+    reveal_voter_details_default == 1
+  end
 end
