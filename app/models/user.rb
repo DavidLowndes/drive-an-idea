cbf4f64fe2cf9d270fc5fcbc45ddf3b700d0edaa
David Lowndes <david.lowndes@firstb2b.com>
2016-10-04 17:00:45 +0100

    Tiny commit before close

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,10 @@ class User < ApplicationRecord
   has_many :votes, dependent: :destroy
 
   def full_name
-    return "#{first_name} #{last_name}".strip if first_name.present? || last_name.present?
-    email # If no name is entered then their email is returned instead
+    if first_name.present? || last_name.present?
+      "#{first_name} #{last_name}".strip 
+    else
+      email # If no name is entered then their email is returned instead
+    end
   end
 end
