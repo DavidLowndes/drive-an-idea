c7eb56ed8a65c0b2b7f43911f823a493dc9f7e7f
David Lowndes <david.lowndes@firstb2b.com>
2016-10-05 15:31:24 +0100

    Fixed my_ideas route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,19 +15,19 @@ class UsersController < ApplicationController
       render status: :not_found, nothing: true
     end
   end
-  
+
   def add_friend
     @friend = User.find(params[:friend])
     current_user.friendships.build(friend_id: @friend.id)
-    
+
     if current_user.save
       redirect_to my_friends_path, notice: "Friend was successfully added"
     else
       redirect_to my_friends_path, flash[:error] = "There was an error adding user as friend"
     end
   end
-  
-  def show
-    @user = User.find(params[:id])
+
+  def my_ideas
+    @user = current_user
   end
 end
