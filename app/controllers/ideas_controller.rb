e75e18f77574e8c2a84228c14e46f4e7caf1050a
David Lowndes <david.lowndes@firstb2b.com>
2016-11-04 09:52:49 +0000

    fixed search

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -10,35 +10,35 @@ class IdeasController < ApplicationController
   end
 
   def open_ideas
-    @search = Idea.all.ransack(params[:q])
+    @search = Idea.ransack(params[:q])
     @ideas = @search.result.order(created_at: :desc).select(&:open?)
   end
   
   def closed_ideas
-    @search = Idea.all.ransack(params[:q])
+    @search = Idea.ransack(params[:q])
     @ideas = @search.result.order(created_at: :desc).select(&:closed?)
   end
   
   def escalated_ideas
-    @search = Idea.all.ransack(params[:q])
+    @search = Idea.ransack(params[:q])
     @ideas = @search.result.order(created_at: :desc)
                            .where(final_verdict: "Escalated")
   end
   
   def discarded_ideas
-    @search = Idea.all.ransack(params[:q])
+    @search = Idea.ransack(params[:q])
     @ideas = @search.result.order(created_at: :desc)
                            .where(final_verdict: "Discarded")
   end
   
   def followed_ideas
-    @search = Idea.all.ransack(params[:q])
+    @search = Idea.ransack(params[:q])
     @ideas = @search.result.where(id: current_user.follows.pluck(:idea_id))
                            .order(created_at: :desc).select(&:open?)
   end
   
   def not_followed_ideas
-    @search = Idea.all.ransack(params[:q])
+    @search = Idea.ransack(params[:q])
     @ideas = @search.result.where.not(id: current_user.follows.pluck(:idea_id))
                            .order(created_at: :desc).select(&:open?)
   end
