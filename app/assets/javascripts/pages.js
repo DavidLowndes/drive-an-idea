c4d120266be34243bd3ffa221262fe5a69e86041
Tobias Leyland <tobias.leyland@firstb2b.com>
2017-07-25 10:46:43 +0100

    Added Yarn, also added Intro.js and some intro stuff

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#start_intro_btn').click(function() {
+    introJs('.start-intro').start()
+  });
+  
+  $('#new_idea_intro_btn').click(function() {
+    introJs('.new-idea-intro').start()
+  });
+});
